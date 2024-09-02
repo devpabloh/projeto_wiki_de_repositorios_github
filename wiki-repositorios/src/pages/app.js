cf9755deff8128e60c18bd6896d32e682c6e92af
Dev. Pablo H. <devpabloh@gmail.com>
2024-09-02 17:42:04 -0300

    atualização

--- wiki-repositorios/src/pages/app.js
+++ wiki-repositorios/src/pages/app.js
@@ -1,9 +1,11 @@
 import gitLogo from "../assets/logo/Github-desktop-logo-symbol.svg.png"
 import {Container} from "./styles"
+import Input from "../components/Input/index"
 function App(){
     return (
         < Container>
             <img src={gitLogo} width={72} height={72} alt="github logo" />
+            <Input/>
         </Container>
     )
 }
--- wiki-repositorios/src/pages/styles.js
+++ wiki-repositorios/src/pages/styles.js
@@ -2,9 +2,11 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     width: 100%;
-    height: 100vh;
+    height: 90vh;
+    margin: 20px 0 0 0;
 
     display: flex;
-    alignItems: flex-start;
-    justify-Content: center;
+    flex-direction: column;
+    align-Items: center;
+    justify-Content: flex-start;
 `
