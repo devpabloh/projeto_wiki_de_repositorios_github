8427d0d61d604300a7a1460bb3bff788b229f277
Dev. Pablo H. <devpabloh@gmail.com>
2024-09-03 13:37:21 -0300

    atualizaçaõ

--- wiki-repositorios/src/pages/app.js
+++ wiki-repositorios/src/pages/app.js
@@ -2,11 +2,13 @@ import gitLogo from "../assets/logo/Github-desktop-logo-symbol.svg.png"
 import {Container} from "./styles"
 import Input from "../components/Input/index"
 import ItemRepo from "../components/ItemRepo";
+import { ButtonContainer } from "../components/Button/styles";
 function App(){
     return (
         < Container>
             <img src={gitLogo} width={72} height={72} alt="github logo" />
             <Input/>
+            <ButtonContainer/>
             <ItemRepo/>
         </Container>
     )
